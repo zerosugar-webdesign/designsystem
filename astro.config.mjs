ed34bbd078734bddb58b5b7a7f9408c70b268d17
Javier Piedra <javierpiedraiglesias@gmail.com>
2026-03-15 09:11:55 +0100

    form + design system

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: netlify(),
     vite: {
         plugins: [tailwindcss()],
     },
